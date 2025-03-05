9d702b89ba41abfdcf4575ee8ea875515f0a0fab
Altiuss <altiuss@inbox.lv>
2025-03-05 08:02:11 +0100

    Logical operators

---
+++ Lesson1/Logical operators/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const hamburger = 2;
+const fries = 1;
+
+
+if (hamburger === 3 && fries ) {
+    console.log("Hallo world")
+} else {
+    console.log("nOT HALLO!");
+}
+
+
+console.log((hamburger && fries));
