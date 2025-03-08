1d24ab00fd3240f9b71ca148a802e90902848646
Altiuss <altiuss@inbox.lv>
2025-03-08 12:53:47 +0100

    Methods and properties of strings and numbers

---
+++ Lesson1/Methods and properties strings and numbers/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const str = "test";
+const arr = [1, 2, 4];
+
+let fruit = "Some fruit";
+
+const logg = "Hallo Aigars!"
+
+const num = 12.6;
+const test = "12.6px"
+console.log(str.length);
+console.log(arr.length);
+
+
+console.log(str.toUpperCase());
+
+
+console.log(fruit.indexOf());
+console.log(logg.slice(6));
+console.log(logg.substring(6));
+console.log();
+console.log(parseInt(test));
+console.log(parseFloat(test));
+
+
+console.log(Math.round(num));
+
+
+
