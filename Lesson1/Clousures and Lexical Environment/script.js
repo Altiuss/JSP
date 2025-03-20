fe90ea5f4a365ed844cba8a122c166c4d4d64717
Altiuss <altiuss@inbox.lv>
2025-03-20 13:06:55 +0100

    Clousures and LexicalEnvironment

---
+++ Lesson1/Clousures and Lexical Environment/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// let number = 5; 
+
+
+// function logNumber(num) {
+//     let number = 4; 
+//     console.log(number);
+// }
+
+// number = 6;
+
+// logNumber(number);
+
+function createCounter() {
+    let counter = 0;
+
+    const myFunction = function () {
+        counter = counter + 1;
+        return counter;
+    }
+    return myFunction;
+}
+
+
+const increment = createCounter(); // clousure(замикание)
+const increment1 = createCounter(); // clousure(замикание)
+const increment2 = createCounter(); // clousure(замикание)
+const increment3 = createCounter(); // clousure(замикание)
+const increment4 = createCounter(); // clousure(замикание)
+const c1 = increment();debugger
+const c2 = increment();debugger
+const c3 = increment();debugger
+const c5 = increment3();debugger
+const c6 = increment4();debugger
+const c7 = increment4();
+const c8 = increment4();
+const c4 = createCounter();
+
+
+console.log(c1, c2, c3, c4,c5,c6,c7,c8);
+
+const myFunction = function () {
+     counter = counter + 1;
+    return counter;
+}
+
+let counter = 0;
+
+// console.log(myFunction());
+// console.log(myFunction());
+// console.log(myFunction());
