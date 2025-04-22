950d7b4a9bea42052cd44c1f46a2204e38fa7e1e
Altiuss <altiuss@inbox.lv>
2025-04-22 16:41:05 +0200

    Creating a countdown timer on the website

--- JavaScript в работе/Создаем табы в новом проекте/Food/src/js/main.js
+++ JavaScript в работе/Создаем табы в новом проекте/Food/src/js/main.js
@@ -1,16 +1,11 @@
 
-import { index } from './../../node_modules/@webassemblyjs/wasm-parser/lib/decoder';
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
     const tabs = document.querySelectorAll('.tabheader__item');
     const tabsContent = document.querySelectorAll('.tabcontent');
     const tabsParent = document.querySelector('.tabheader__items');
-
-    console.log('tabs:', tabs);
-    console.log('tabsContent:', tabsContent);
-
-
 
 
 
@@ -29,9 +24,6 @@ document.addEventListener('DOMContentLoaded', () => {
         tabs[i].classList.add('tabheader__item_active');
     }
 
-    
-    
-
     tabsParent.addEventListener('click', (e) => {
         const target = e.target;
         if (target && target.classList.contains('tabheader__item')) {
@@ -40,18 +32,68 @@ document.addEventListener('DOMContentLoaded', () => {
                     hideTabContent();
                     showTabContent(index);
                 }
-                
+
             })
         }
 
-    })
+    });
 
-    hideTabContent();
+    // Timer
 
-    showTabContent();
+    const deadLine = '2025-04-31';
 
 
+    function getTimeRemaining(endTime) {
+        const t = Date.parse(endTime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor(t / (1000 * 60 * 60) % 24),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+            
+           return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
 
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endTime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+
+        function updateClock() {
+            const t = getTimeRemaining(endTime);
+
+            days.textContent = getZero(t.days);
+            hours.textContent = getZero(t.hours);
+            minutes.textContent = getZero(t.minutes);
+            seconds.textContent = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+
+        }
+
+    }
+    setClock('.timer', deadLine);
 
 
 
